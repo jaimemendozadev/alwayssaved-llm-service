ecb388e380dd8169905237cf28b49667cb97a52d
jaimemendozadev <5395811+jaimemendozadev@users.noreply.github.com>
2025-06-27 08:26:32 -0700

    Move main.py to /server folder
    Start scaffolding /server folders and
    folder/module architecture.

--- service.py
+++ service.py
@@ -3,4 +3,4 @@ import uvicorn
 if __name__ == "__main__":
 
     print("Starting app. 🚀")
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("server.main:app", host="127.0.0.1", port=8000, reload=True)
