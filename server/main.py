e3404526371d64a71a8aced00d74532138712e61
jaimemendozadev <5395811+jaimemendozadev@users.noreply.github.com>
2025-06-28 11:24:18 -0700

    Add middleware to Backend server
    Also added/readded dependencies.

--- server/main.py
+++ server/main.py
@@ -1,6 +1,23 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from server.routes.llm import llm_router
 
 app = FastAPI()
+
+
+# Add CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:3000"
+    ],  # <-- For development; restrict this in prod
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(llm_router, prefix="/api")
 
 
 @app.get("/")
