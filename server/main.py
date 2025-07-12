bb8103520e41cde322e5ab3904f839dbfdc55d80
jaimemendozadev <5395811+jaimemendozadev@users.noreply.github.com>
2025-07-11 20:11:09 -0700

    Renamed llm_router to convos_router

--- server/main.py
+++ server/main.py
@@ -3,23 +3,28 @@ import os
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-from server.routes.llm import llm_router
+from server.routes.convos import convos_router
 
 app = FastAPI()
 
-APP_DOMAIN = os.getenv("APP_DOMAIN")
+PYTHON_MODE = os.getenv("PYTHON_MODE", "DEVELOPMENT")
 
+APP_DOMAIN = (
+    os.getenv("PRODUCTION_APP_DOMAIN", "")
+    if PYTHON_MODE == "PRODUCTION"
+    else os.getenv("DEVELOPMENT_APP_DOMAIN", "")
+)
 
 # Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[APP_DOMAIN],  # <-- For development; restrict this in prod
+    allow_origins=[APP_DOMAIN],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-app.include_router(llm_router, prefix="/api")
+app.include_router(convos_router, prefix="/api")
 
 
 @app.get("/")
--- server/routes/convos.py
+++ server/routes/convos.py
@@ -1,5 +1,3 @@
-from typing import List
-
 from fastapi import APIRouter, Depends
 from fastapi.exceptions import RequestValidationError
 from pydantic import BaseModel
@@ -8,24 +6,27 @@ from server.utils.clerk import clerk_authenticate_get_user_details
 
 
 class LLMRequestBody(BaseModel):
-    file_ids: List[str]
+    conversation_id: str
+    user_id: str
+    sender_type: str
     message: str
 
 
-llm_router = APIRouter(
-    prefix="/notes", dependencies=[Depends(clerk_authenticate_get_user_details)]
+convos_router = APIRouter(
+    prefix="/convos", dependencies=[Depends(clerk_authenticate_get_user_details)]
 )
 
 
-@llm_router.post("/{note_id}")
-async def query_llm(body: LLMRequestBody, note_id: str):
-    print(f"note_id: {note_id}")
+@convos_router.post("/{convo_id}")
+async def query_llm(body: LLMRequestBody, convo_id: str):
+    print(f"convo_id: {convo_id}")
     print(f"body: {body}")
     try:
-        print(f"note_id: {note_id}")
-        print(f"body: {body}")
-        print(f"file_ids: {body.file_ids}")
+        print(f"conversation_id: {body.conversation_id}")
+        print(f"user_id: {body.user_id}")
+        print(f"sender_type: {body.sender_type}")
         print(f"message: {body.message}")
+
         return {"status": 200, "message": "IT WORKS! 🚀"}
     except RequestValidationError as e:
         print("Validation error:", e.errors())
