9b9a1f37c0d1434d28ae1aa6b31ea05a91b3aab3
jaimemendozadev <5395811+jaimemendozadev@users.noreply.github.com>
2025-07-13 06:40:56 -0700

    Add query_qdrant_with_message function

--- server/utils/llm/mistral.py
+++ server/utils/llm/mistral.py
@@ -1,18 +1,14 @@
 import os
 from typing import List
 
-import numpy as np
 from mistralai import Mistral
-from qdrant_client import QdrantClient
 from qdrant_client.http.models.models import ScoredPoint
-from sentence_transformers import SentenceTransformer
 
 from server.utils.aws.ssm import get_secret
-from server.utils.embedding import chunk_text, get_embedd_model
-from server.utils.qdrant import get_qdrant_client
+from server.utils.qdrant import query_qdrant_with_message
 
 QDRANT_COLLECTION_NAME = os.getenv("QDRANT_COLLECTION_NAME", "alwayssaved_user_files")
-MISTRAL_MODEL = os.getenv("MISTRAL_MODEL", "mistral-7b-instruct-v0.1")
+MISTRAL_MODEL = os.getenv("MISTRAL_MODEL", "open-mistral-7b")
 MISTRAL_API_KEY = get_secret("/alwayssaved/MISTRAL_API_KEY")
 
 mistral_client = Mistral(api_key=MISTRAL_API_KEY)
@@ -42,34 +38,21 @@ def generate_prompt(qdrant_results: List[ScoredPoint], message: str) -> str:
 
 
 def query_llm(message: str) -> str:
-    embedding_model: SentenceTransformer = get_embedd_model()
-    qdrant_client: QdrantClient = get_qdrant_client()
 
-    # Chunk & vectorize incoming message
-    chunks = chunk_text(message)
-
-    vectors = embedding_model.encode(chunks, normalize_embeddings=True)
-
-    query_vector = np.mean(vectors, axis=0)
-
-    # Search Qdrant Database for similar vectors with user message
-    hits = qdrant_client.search(
-        collection_name=QDRANT_COLLECTION_NAME,
-        query_vector=query_vector.tolist(),
-        limit=5,  # Return 5 closest points
-    )
+    hits = query_qdrant_with_message(message)
 
     # print(f"hits in query_llm: {hits}")
 
     if len(hits) > 0:
         prompt = generate_prompt(hits, message)
-        # messages = [
-        #     {"role": "user", "content": prompt}
-        #     ]
-        # chat_response = mistral_client.chat.complete(
-        #     model=MISTRAL_MODEL,
-        #     messages=messages
-        #     )
-        # print(f"chat_response: {chat_response}")
+        messages = [{"role": "user", "content": prompt}]
+        chat_response = mistral_client.chat.complete(
+            model=MISTRAL_MODEL, messages=messages
+        )
+
+        print(f"chat_response type: {type(chat_response)}")
+        print(f"chat_response: {chat_response}")
+        return "Success"
+
     else:
-        return "Error"
+        return "I'm sorry, but we can't generate an answer. 😔 Please try asking in another way."
--- server/utils/qdrant.py
+++ server/utils/qdrant.py
@@ -1,10 +1,15 @@
 import os
+from typing import List
 
+import numpy as np
 from qdrant_client import QdrantClient
 from qdrant_client.conversions.common_types import CollectionInfo
 from qdrant_client.http.exceptions import UnexpectedResponse
+from qdrant_client.http.models.models import ScoredPoint
+from sentence_transformers import SentenceTransformer
 
 from server.utils.aws.ssm import get_secret
+from server.utils.embedding import chunk_text, get_embedd_model
 
 QDRANT_COLLECTION_NAME = os.getenv("QDRANT_COLLECTION_NAME", "alwayssaved_user_files")
 
@@ -37,3 +42,24 @@ def get_qdrant_collection(q_client: QdrantClient) -> CollectionInfo | None:
         print(f"❌ QdrantClient UnexpectedResponse Error in get_qdrant_collection: {e}")
 
     return None
+
+
+def query_qdrant_with_message(message: str) -> List[ScoredPoint]:
+    embedding_model: SentenceTransformer = get_embedd_model()
+    qdrant_client: QdrantClient = get_qdrant_client()
+
+    # Chunk & vectorize incoming message
+    chunks = chunk_text(message)
+
+    vectors = embedding_model.encode(chunks, normalize_embeddings=True)
+
+    query_vector = np.mean(vectors, axis=0)
+
+    # Search Qdrant Database for similar vectors with user message
+    hits = qdrant_client.search(
+        collection_name=QDRANT_COLLECTION_NAME,
+        query_vector=query_vector.tolist(),
+        limit=5,  # Return 5 closest points
+    )
+
+    return hits
