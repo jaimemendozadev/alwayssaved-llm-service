29231488d3344ddafd1c853824a1476c40c0fda8
jaimemendozadev <5395811+jaimemendozadev@users.noreply.github.com>
2025-07-13 10:11:05 -0700

    Add ConvoMessage doc creation in handle_incoming_user_message
    Need to persist an incoming user message and the
    response from the LLM, if any, in the database.

--- server/routes/convos.py
+++ server/routes/convos.py
@@ -1,6 +1,10 @@
+import os
+
+from bson.objectid import ObjectId
 from fastapi import APIRouter, Depends
 from fastapi.exceptions import RequestValidationError
 from pydantic import BaseModel
+from pymongo.errors import ServerSelectionTimeoutError
 
 from server.utils.clerk import authenticate_clerk_user
 from server.utils.llm.mistral import BASE_LLM_NO_RESPONSE, query_llm
@@ -19,6 +23,9 @@ convos_router = APIRouter(
     prefix="/convos", dependencies=[Depends(authenticate_clerk_user)]
 )
 
+LLM_COMPANY = os.getenv("LLM_COMPANY", "Unknown")
+LLM_MODEL = os.getenv("LLM_MODEL", "Unknown")
+
 
 @convos_router.post("/{convo_id}")
 async def handle_incoming_user_message(body: ConvoPostRequestBody, convo_id: str):
@@ -27,24 +34,63 @@ async def handle_incoming_user_message(body: ConvoPostRequestBody, convo_id: str
 
     user_id = body.user_id
     conversation_id = body.conversation_id
+    user_id = body.user_id
     try:
         message = body.message
 
         mongo_client = create_mongodb_instance()
+        if mongo_client is None:
+            raise RuntimeError("Failed to connect to MongoDB")
 
-        qdrant_hits = query_qdrant_with_message(
-            message, message, user_id, conversation_id
+        convo_msg_collection = mongo_client.get_database("alwayssaved").get_collection(
+            "convomessages"
         )
+
+        await convo_msg_collection.insert_one(
+            {
+                "conversation_id": ObjectId(conversation_id),
+                "user_id": ObjectId(user_id),
+                "sender_type": "user",
+                "message": message,
+            }
+        )
+
+        qdrant_hits = query_qdrant_with_message(message, user_id, conversation_id)
 
         if len(qdrant_hits) > 0:
             llm_response = query_llm(qdrant_hits, message, user_id, conversation_id)
-            return {"status": 200, "message": llm_response}
 
-        mongo_client.get_database("alwayssaved").get_collection("users").find_one(
-            {"clerk_id": clerk_id}
+            llm_message = await convo_msg_collection.insert_one(
+                {
+                    "conversation_id": ObjectId(conversation_id),
+                    "user_id": ObjectId(user_id),
+                    "sender_type": "llm",
+                    "message": llm_response,
+                    "llm_info": {"llm_company": LLM_COMPANY, "llm_model": LLM_MODEL},
+                }
+            )
+            return {"status": 200, "payload": llm_message}
+
+        llm_error_message = await convo_msg_collection.insert_one(
+            {
+                "conversation_id": ObjectId(conversation_id),
+                "user_id": ObjectId(user_id),
+                "sender_type": "llm",
+                "message": BASE_LLM_NO_RESPONSE,
+                "llm_info": {"llm_company": LLM_COMPANY, "llm_model": LLM_MODEL},
+            }
         )
 
-        return {"status": 200, "message": BASE_LLM_NO_RESPONSE}
+        return {"status": 200, "payload": llm_error_message}
+
+    except RuntimeError:
+        print("MongoDB connection timed out.")
+        return {"status": 503, "message": "Database unavailable"}
+
+    except ServerSelectionTimeoutError:
+        print("MongoDB connection timed out.")
+        return {"status": 503, "message": "Database unavailable"}
+
     except RequestValidationError as e:
         print(f"RequestValidationError for Convo {convo_id}: ", e.errors())
 
